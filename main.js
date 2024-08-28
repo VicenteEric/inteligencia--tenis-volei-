e1be537eac608849416e23413b8d12e754331835
Eric Henrique Alves Vicente <106555851+VicenteEric@users.noreply.github.com>
2024-08-28 11:22:56 -0300

    mestre

--- main.js
+++ main.js
@@ -82,11 +82,16 @@ const perguntas = [
     }
 ];
 
+
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
 
-function mostraPergunta(){
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     caixaAlternativas.textContent = "";
@@ -94,7 +99,7 @@ function mostraPergunta(){
 }
 
 function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas);{
+    for(const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
         botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
@@ -107,6 +112,12 @@ function respostaSelecionada(opcaoSelecionada) {
     historiaFinal += afirmacoes + " ";
     atual++;
     mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
 }
 
 mostraPergunta();
